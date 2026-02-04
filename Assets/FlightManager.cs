214950fa989aeb2ab3709973517caa5dd5aba915
maybelizard <paiculiz9608@gmail.com>
2026-02-04 03:57:39 +0200

    Fixed landing and FlyingOverCity

---
+++ Assets/FlightManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class FlightManager
+{
+    // This lives in memory and won't vanish until you close the game
+    public static bool isReturningForLanding = false;
+}
